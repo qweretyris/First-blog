169c13942a74a22e080b0e6e157629e7ebad4833
qweretyris123 <kkerlton17@gmail.com>
2022-12-26 23:02:46 +0200

    My first blog

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ApplicationRecord
+	
+	has_many :comments, dependent: :destroy
+	belongs_to :user
+
+	validates :title, :user, presence: true
+	validates :body, presence: true, length: { minimum: 10 }
+end
